9e92fca397700c576e794f76cb43a647f21a5468
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-08-12 23:30:50 -0300

    Criando edição

---
+++ 10_MVC/9_10_editando_dados/controllers/TaskController.js
@@ -0,0 +1,54 @@
+const Task = require('../models/Task')
+
+module.exports = class TaskController {
+
+    static createTask(req, res) {
+        res.render('tasks/create')
+    }
+
+    static async createTaskSave(req, res) {
+        const { title, description } = req.body
+
+        const task = {
+            title,
+            description,
+            done: false
+        }
+
+        await Task.create(task)
+
+        res.redirect('/tasks')
+    }
+
+    static async showTask(req, res) {
+        const task = await Task.findAll({ raw: true })
+        res.render('tasks/all', { task })
+
+    }
+
+    static async removeTask(req, res) {
+        const id = req.body.id
+
+        await Task.destroy({ where: { id: id } })
+
+        res.redirect('/tasks')
+    }
+
+    static async updataTask(req, res) {
+        const id = req.params.id
+        const task = await Task.findOne({ where: { id: id }, raw: true })
+
+        res.render('tasks/edit', { task })
+    }
+
+    static async updataTaskPost(req, res) {
+        const { id, title, description } = req.body
+        const task = {
+            title,
+            description
+        }
+        await Task.update( task,{ where: {id: id}})
+
+        res.redirect('/tasks')
+    }
+}
---
+++ 10_MVC/9_10_editando_dados/routes/tasksRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+const TaskController = require('../controllers/TaskController')
+
+
+router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.removeTask)
+router.get('/edit/:id', TaskController.updataTask)
+router.post('/edit', TaskController.updataTaskPost)
+router.get('/', TaskController.showTask)
+
+module.exports = router
