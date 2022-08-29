d1f077f128b706f02ed3bf4fe922d0f9dd0730f9
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-08-29 19:43:07 -0300

    Aula 7 - Editando produto

---
+++ 12_MongoDB/7_editar dados/Controllers/ProductController.js
@@ -0,0 +1,58 @@
+const Product = require('../Models/Product')
+
+module.exports = class ProductController {
+    static async showProducts(req, res) {
+        const products = await Product.getProducts()
+        res.render('products/all', { products })
+    }
+    static async createProduct(req, res) {
+        res.render('products/create')
+    }
+    static async showProductsPost(req, res) {
+        const { name, image, price, description } = req.body
+
+        const product = new Product(name, image, price, description)
+
+        product.save()
+
+        res.redirect('/products')
+    }
+
+    static async getProduct(req, res){
+        const id = req.params.id
+
+        const product = await Product.getProductByID(id)
+
+        res.render('products/product', { product })
+    }
+
+    static async deleteProduct(req, res){
+        const id = req.params.id
+
+         await Product.deleteProductByID(id)
+
+        res.redirect('/products')
+    }
+
+    static async editProduct(req, res){
+        const id = req.params.id
+        
+        const product = await Product.getProductByID(id)
+
+        console.log(product)
+
+        res.render('products/edit', { product })
+    }
+
+    static async editProductPost(req, res){
+
+        const { id, name, image, price, description } = req.body
+
+        const product = new Product(name, image, price, description)
+
+        await product.updateProduct(id)
+
+        res.redirect('/products')
+    }
+
+}
